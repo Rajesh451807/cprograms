1a1a91ba48a971895f1f5d909d30bc9ec978d1c8
Rajesh451807 <rajeshpalla489@gmail.com>
2024-10-21 11:57:20 +0530

    Create quadratic.c

---
+++ quadratic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+int a,b,c,d;
+float r1,r2;
+printf("enter a,b,c");
+scanf("%d,%d,%d", &a,&b,&c);
+if(d>0)
+printf("r1=%f,r2=5f",r1,r2);
+else if(d<0)
+{
+    printf("roots are imaginary");
+    {
+        
+    {
+        printf("roots are real and equal");
+        
+    }
+    }
+}
+}
