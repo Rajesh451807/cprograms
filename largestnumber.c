15328c2dc45d176047dbda0277280a526f797d91
Rajesh451807 <rajeshpalla489@gmail.com>
2024-10-21 12:06:50 +0530

    Create largestnumber.c

---
+++ largestnumber.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the values of a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    {
+        if(a>c)
+        {
+            printf("%d is largest",a);
+        }
+        else if(b>c)
+        {
+            printf("%d is largest",b);
+        }
+        else
+        {
+            printf("%d is largest",c)
+        }
+    }
+}
