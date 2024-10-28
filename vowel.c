4e4413afbae3d974390b05c70498c250c67ba068
Rajesh451807 <rajeshpalla489@gmail.com>
2024-10-28 12:09:44 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+    char ch;
+    printf("enter the character");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case'a':
+        case'e':
+        case'i':
+        case'o':
+        case'u':
+        case'A':
+        case'E':
+        case'I':
+        case'O':
+        case'U':
+        printf("%c is a vowel",ch);
+        break;
+        default:("%c is a consonant",ch);
+        break;
+        
+    }
+}
