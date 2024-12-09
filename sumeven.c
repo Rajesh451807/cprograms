76352edd586a388e9c47e0d710a8a595cf447991
Rajesh451807 <rajeshpalla489@gmail.com>
2024-12-09 12:13:37 +0530

    Create sumeven.c

---
+++ sumeven.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i,n,sum;
+    printf("enter n value");
+    scanf("%d",&n);
+    sum=0;
+for(i=1;i<=n;++i)
+    {
+if(i%2==0);
+    sum=sum+i;
+    }
+printf("%d",sum);
+    
+}
