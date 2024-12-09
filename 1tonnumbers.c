bf3075f60ae1bd3c5a6133793b1aafab891b643b
Rajesh451807 <rajeshpalla489@gmail.com>
2024-12-09 12:05:01 +0530

    Create 1tonnumbers.c

---
+++ 1tonnumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i,n,sum;
+    printf("enter n value");
+    scanf("%d",&n);
+    sum=0;
+
+    for(i=15;i=n;++i)
+    {
+        sum=sum+i;
+    }
+        printf("%d",i);
+    
+}
