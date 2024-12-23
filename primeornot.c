7dd8e4c26eec4a3284d82bf9f8f3d49e9e63bbc1
Rajesh451807 <rajeshpalla489@gmail.com>
2024-12-23 11:48:06 +0530

    Create primeornot.c

---
+++ primeornot.c
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int i,n,count=0;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {
+        if(n%i==0)
+        count++;
+    }
+    if(count==2)
+    {
+    printf("prime");
+}
+    else
+{
+    printf("not prime");
+}
+}
