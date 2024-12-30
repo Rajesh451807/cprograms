ef1ff5f3c0493f454d547efe49110f35b2d95d88
Rajesh451807 <rajeshpalla489@gmail.com>
2024-12-30 12:31:04 +0530

    Create Nestedloop6.c

---
+++ Nestedloop6.c
@@ -0,0 +1,23 @@
+include <stdio.h>
+void main() 
+{
+    int i,j,k;
+    for(i=0;i<=5;i++)
+    {
+    for(j=i;j<i;j++)
+    {
+        printf("*");
+    }
+    for(i=0;i<=5;i++)
+    {
+   for(j=5;j>5;--j)
+   {
+    printf("*");
+    }
+    printf("\n");
+    }
+        
+    }
+    
+}
+
