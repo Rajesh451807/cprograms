b9ab384fd0d0ce2ffed343eb0febd22b4247110b
Rajesh451807 <rajeshpalla489@gmail.com>
2025-01-20 12:16:10 +0530

    Create Stringandprint.c

---
+++ Stringandprint.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[20];
+    printf("Enter your first name: ");
+    scanf("%s", str);
+    printf("Hello, %s", str);
+   
+    return 0;
+}
